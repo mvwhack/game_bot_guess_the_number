5913841d2671d58a8b4427f408a198e458d9aaab
Vlad <vmansurov@icloud.com>
2020-10-20 17:25:58 +0300

    Выполнил полсностью ДЗ 6 урока, вмесет со сложным заданием

---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// Функция проверки введеного аргумента на число
+let isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let randomNumber = parseInt(Math.random() * 100);
+let count = 10;
+// Функция отвечающая за ход игры
+function guessNumber() {
+    if(count > 0){
+        let userNumber = prompt('Отгадайте случайное число. Введите свой варианет от 1 до 100');
+        if (isNumber(userNumber)) {
+            userNumber = +userNumber;
+            count--;
+            if (userNumber > randomNumber) {
+                alert('Загаданное число меньше, у вас осталось ' +count+ ' попыток');
+            } else if (userNumber < randomNumber) {
+                alert('Загаданное число больше, у вас осталось ' +count+ ' попыток');
+            } else if (userNumber === randomNumber) {
+                let again = confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще раз?');
+                if(again === true){
+                    guessNumber();
+                } else {
+                    alert('Было приятно с тобой играть.');
+                    return;
+                } 
+            }
+        } else {
+            alert('Можно вводить только числа');
+        }
+        return guessNumber();
+    } else {
+        let again2 = confirm('У вас закончились поппытки! Хотите сыграть еще раз?');
+        if(again2 === true){
+            guessNumber();
+        } else {
+            alert('Было приятно с тобой играть.');
+            return;
+        }
+    }
+}
+
+// Функция узнет у пользователя, хочет он играть или нет.
+function startGame(){
+    let start = confirm('Првивет. Это игра угадайка. Она задумала случайное число которое нужно отгадать. Попробуешь свои силы?');
+    if(start === true){
+        guessNumber();
+    } else {
+        alert('Игра окончена');
+    }
+}
+
+startGame();
+
+
