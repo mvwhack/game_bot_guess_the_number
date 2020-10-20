19fa357ecadf4a863c4afcb9ada1a1a758d8788c
Vlad <vmansurov@icloud.com>
2020-10-20 23:59:44 +0300

    Исправил ошибку с размещением переменных

--- js/main.js
+++ js/main.js
@@ -5,53 +5,53 @@ let isNumber = function(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let randomNumber = parseInt(Math.random() * 100);
-let count = 10;
-// Функция отвечающая за ход игры
-function guessNumber() {
-    if(count > 0){
-        let userNumber = prompt('Отгадайте случайное число. Введите свой варианет от 1 до 100');
-        if (isNumber(userNumber)) {
-            userNumber = +userNumber;
-            count--;
-            if (userNumber > randomNumber) {
-                alert('Загаданное число меньше, у вас осталось ' +count+ ' попыток');
-            } else if (userNumber < randomNumber) {
-                alert('Загаданное число больше, у вас осталось ' +count+ ' попыток');
-            } else if (userNumber === randomNumber) {
-                let again = confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще раз?');
-                if(again === true){
-                    guessNumber();
-                } else {
-                    alert('Было приятно с тобой играть.');
-                    return;
-                } 
+function botRundom(){
+    let randomNumber = parseInt(Math.random() * 100),
+        count = 10;
+    // Функция отвечающая за ход игры
+    function guessNumber() {
+        if(count > 0){
+            let userNumber = prompt('Отгадайте случайное число. Введите свой варианет от 1 до 100');
+            if (isNumber(userNumber)) {
+                userNumber = +userNumber;
+                count--;
+                if (userNumber > randomNumber) {
+                    alert('Загаданное число меньше, у вас осталось ' +count+ ' попыток');
+                } else if (userNumber < randomNumber) {
+                    alert('Загаданное число больше, у вас осталось ' +count+ ' попыток');
+                } else if (userNumber === randomNumber) {
+                    let again = confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще раз?');
+                    if(again === true){
+                        botRundom();
+                    } else {
+                        alert('Было приятно с тобой играть.');
+                        return again === false;
+                    } 
+                }
+            } else {
+                alert('Можно вводить только числа');
             }
+            return guessNumber();
         } else {
-            alert('Можно вводить только числа');
+            let again2 = confirm('У вас закончились поппытки! Хотите сыграть еще раз?');
+            if(again2 === true){
+                botRundom();
+            } else {
+                alert('Было приятно с тобой играть.');
+                return;
+            }
         }
-        return guessNumber();
-    } else {
-        let again2 = confirm('У вас закончились поппытки! Хотите сыграть еще раз?');
-        if(again2 === true){
+    }
+
+    // Функция узнет у пользователя, хочет он играть или нет.
+    function startGame(){
+        let start = confirm('Првивет. Это игра угадайка. Она задумала случайное число которое нужно отгадать. Попробуешь свои силы?');
+        if(start === true){
             guessNumber();
         } else {
-            alert('Было приятно с тобой играть.');
-            return;
+            alert('Игра окончена');
         }
     }
+    startGame();
 }
-
-// Функция узнет у пользователя, хочет он играть или нет.
-function startGame(){
-    let start = confirm('Првивет. Это игра угадайка. Она задумала случайное число которое нужно отгадать. Попробуешь свои силы?');
-    if(start === true){
-        guessNumber();
-    } else {
-        alert('Игра окончена');
-    }
-}
-
-startGame();
-
-
+botRundom();
